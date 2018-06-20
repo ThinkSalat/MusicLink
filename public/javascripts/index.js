bc67467ded5478712a1a98be121c0d999251c3bb
Shawn Salat <shawn.salat@gmail.com>
2018-06-19 20:29:42 -0700

    refactor autocomplete, move to folder

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,5 @@
 import { search, retrieveArtist, retrieveRelatedArtists, retrieveArtistBio, getArtistBio, getArtistBioByName } from '../../util/util';
+import { createAutocompleteList } from '../../util/search_util';
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -29,44 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
       .catch( err => console.log('err', err));
   });
 
-  const createAutocompleteList = artists => {
-    if (!artists || !artists.length) {
-      const emptyList = document.createElement('ul');
-      const emptyListItem = document.createElement('li');
-      emptyListItem.innerHTML = "Couldn't find artist with that name";
-      emptyList.appendChild(emptyListItem);
-      return emptyList;
-    }
-    const ul = document.createElement('ul');
-    ul.setAttribute('id','name-list');
-    artists.forEach( artist => {
-      const listItem = createListItem(artist);
-      ul.appendChild(listItem);
-    });
-    console.log(ul);
-    return ul;
-  };
-
-  const createListItem = artist => {
-    const listItem = document.createElement('li');
-    listItem.setAttribute('id', 'list-item');
-
-    const artistImage = document.createElement('img');
-    artistImage.setAttribute('class', 'list-image');
-    if (artist.imageUrl === 'default') {
-      artistImage.setAttribute('src', 'http://groovesharks.org/assets/images/default_avatar.jpg');
-    } else {
-      artistImage.setAttribute('src', artist.imageUrl);
-    }
-
-    const artistName = document.createElement('span');
-    artistName.innerHTML = artist.name;
-
-    listItem.appendChild(artistImage);
-    listItem.appendChild(artistName);
-
-    return listItem;
-  };
 
 window.search = search;
 window.retrieveArtist = retrieveArtist;
---
+++ util/search_util.js
@@ -0,0 +1,38 @@
+export const createAutocompleteList = artists => {
+  if (!artists || !artists.length) {
+    const emptyList = document.createElement('ul');
+    const emptyListItem = document.createElement('li');
+    emptyListItem.innerHTML = "Couldn't find artist with that name";
+    emptyList.appendChild(emptyListItem);
+    return emptyList;
+  }
+  const ul = document.createElement('ul');
+  ul.setAttribute('id','name-list');
+  artists.forEach( artist => {
+    const listItem = createListItem(artist);
+    ul.appendChild(listItem);
+  });
+  console.log(ul);
+  return ul;
+};
+
+export const createListItem = artist => {
+  const listItem = document.createElement('li');
+  listItem.setAttribute('id', 'list-item');
+
+  const artistImage = document.createElement('img');
+  artistImage.setAttribute('class', 'list-image');
+  if (artist.imageUrl === 'default') {
+    artistImage.setAttribute('src', 'http://groovesharks.org/assets/images/default_avatar.jpg');
+  } else {
+    artistImage.setAttribute('src', artist.imageUrl);
+  }
+
+  const artistName = document.createElement('span');
+  artistName.innerHTML = artist.name;
+
+  listItem.appendChild(artistImage);
+  listItem.appendChild(artistName);
+
+  return listItem;
+};
