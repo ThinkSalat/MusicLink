f6b01c972dcc2b4ca517eb594126db063eddf084
Shawn Salat <shawn.salat@gmail.com>
2018-06-21 23:43:08 -0700

    images showing up on nodes

--- public/javascripts/d3.js
+++ public/javascripts/d3.js
@@ -41,17 +41,72 @@ export const createD3 = () => {
   let width = window.innerWidth,
   height = window.innerHeight;
 
-  // set svg to be height and width
   const svg = d3.select('#d3-canvas')
     .append('svg')
-    .attr('width', width)
     .attr('height', height)
-    .data(nodes);
+    .attr('width', width)
+    .append('g')
+    .attr('transform', `translate(${width/2},${height/2})`);
+
+
+  var defs = svg.append('defs');
+
+  defs.append('pattern')
+    
+
+    // the simulation is a collection of forces about our simulation
+  const simulation = d3.forceSimulation()
+    .force('centerX', d3.forceX().strength(0.05))
+    .force('centerY', d3.forceY().strength(0.05))
+    .force('collide', d3.forceCollide(45));
+
+  const circles = svg.selectAll('circle')
+    .data(nodes)
+    .enter().append('circle')
+    .attr('r', 40)
+    .attr('cy', 100)
+    .attr('cx', 300)
+    .attr('fill', node => `url(#${node.id})`)
+    .attr('class', 'node')
+    .on('click', node => node.onClick());
+
+  defs.selectAll('.artist-pattern')
+    .data(nodes)
+    .enter().append('pattern')
+    .attr('class', 'artist-pattern')
+    .attr('id', node => node.id)
+    .attr('height', '100%')
+    .attr('width', '100%')
+    .attr('patternContentUnits', 'objectBoundingBox')
+    .append('image')
+    .attr('width', 1)
+    .attr('height', 1)
+    .attr('preserveAspectRatio', 'none')
+    .attr('xmlns:link', "http://www.w3.org/1999/xlink")
+    .attr('xlink:href', node => node.images[2].url);
+
+
+
+    simulation.nodes(nodes)
+      .on('tick', ticked);
+
+    function ticked() {
+      circles
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y);
+    }
+
+  // set svg to be height and width
+  // const svg = d3.select('#d3-canvas')
+  //   .append('svg')
+  //   .attr('width', width)
+  //   .attr('height', height)
+  //   .data(nodes);
 
   // set force simulation
-  const simulation = d3.forceSimulation()
-    .force('charge', d3.forceManyBody().strength(-15)) 
-    .force('center', d3.forceCenter(width / 2, height / 2));
+  // const simulation = d3.forceSimulation()
+  //   .force('charge', d3.forceManyBody().strength(-15)) 
+  //   .force('center', d3.forceCenter(width / 2, height / 2));
 
   // select colors
   function getNodeColor(node) {
@@ -68,70 +123,70 @@ export const createD3 = () => {
   }
 
   // NODES
-  const nodeElements = svg.append('g')
-    .selectAll('circle')
-    .data(nodes)
-    .enter().append('circle')
-      .attr('r', 25)
-      .attr('fill', getNodeColor)
-      .attr('class', 'node')
-      .on('click', node => node.onClick());
+  // const nodeElements = svg.append('g')
+  //   .selectAll('circle')
+  //   .data(nodes)
+  //   .enter().append('circle')
+  //     .attr('r', 25)
+  //     .attr('fill', getNodeColor)
+  //     .attr('class', 'node')
+  //     .on('click', node => node.onClick());
 
-  const textElements = svg.append('g')
-    .selectAll('text')
-    .data(nodes)
-    .enter().append('text')
-      .text(node => node.name)
-      .attr('font-size', 15)
-      .attr('dx', 5)
-      .attr('dy', 37)
-      .attr('class', 'node-text')
-      .style('fill', '#e2e5e4')
-      .style("text-anchor", "middle");
+  // const textElements = svg.append('g')
+  //   .selectAll('text')
+  //   .data(nodes)
+  //   .enter().append('text')
+  //     .text(node => node.name)
+  //     .attr('font-size', 15)
+  //     .attr('dx', 5)
+  //     .attr('dy', 37)
+  //     .attr('class', 'node-text')
+  //     .style('fill', '#e2e5e4')
+  //     .style("text-anchor", "middle");
 
   // IMAGES
 
-  var images = svg.select('circle').enter().append("svg:image")
-    .attr("xlink:href", node => node.images[2].url)
-    .attr("x", d => d.x)
-    .attr("y", d => d.y)
-    .attr("height", 50)
-    .attr("width", 50);
+  // const images = svg.select('circle').enter().append("svg:image")
+  //   .attr("xlink:href", node => node.images[2].url)
+  //   .attr("x", d => d.x)
+  //   .attr("y", d => d.y)
+  //   .attr("height", 50)
+  //   .attr("width", 50);
 
 
 
     // FORCE SIM
-  simulation.nodes(nodes).on('tick', () => {
-    nodeElements
-      .attr('cx', node => node.x)
-      .attr('cy', node => node.y);
-    textElements
-      .attr('x', node => node.x)
-      .attr('y', node => node.y);
-  });
+  // simulation.nodes(nodes).on('tick', () => {
+  //   nodeElements
+  //     .attr('cx', node => node.x)
+  //     .attr('cy', node => node.y);
+  //   textElements
+  //     .attr('x', node => node.x)
+  //     .attr('y', node => node.y);
+  // });
 
 // LINKS
 
-  simulation.force('link', d3.forceLink()
-    .id(link => link.id)
-    .strength(link => link.strength));
+  // simulation.force('link', d3.forceLink()
+  //   .id(link => link.id)
+  //   .strength(link => link.strength));
 
-  const linkElements = svg.append('g')
-  .selectAll('line')
-  .data(links)
-  .enter().append('line')
-    .attr('stroke-width', 1)
-    .attr('stroke', '#E5E5E5');
+  // const linkElements = svg.append('g')
+  // .selectAll('line')
+  // .data(links)
+  // .enter().append('line')
+  //   .attr('stroke-width', 1)
+  //   .attr('stroke', '#E5E5E5');
 
-  linkElements
-  .attr('x1', link => {
-    return link.source.x
-  })
-  .attr('y1', link => link.source.y)
-  .attr('x2', link => link.target.x)
-  .attr('y2', link => link.target.y);
+  // linkElements
+  // .attr('x1', link => {
+  //   return link.source.x
+  // })
+  // .attr('y1', link => link.source.y)
+  // .attr('x2', link => link.target.x)
+  // .attr('y2', link => link.target.y);
 
-  simulation.force('link').link(links);
+  // simulation.force('link').link(links);
 
 
 
